30cf843684b08c9d38cefd5bc922a5660d98cb2d
Vasileios Kochliaridis <kohliaridis@Hotmail.com>
2021-04-13 12:42:26 +0300

    Update mnist_outliers_removal.py

--- Visualization-Outlier-Removal/mnist_outliers_removal.py
+++ Visualization-Outlier-Removal/mnist_outliers_removal.py
@@ -1,5 +1,5 @@
 from keras.datasets import mnist
-from sklearn.cluster import DBSCAN
+import hdbscan
 import matplotlib.pyplot as plt
 import numpy as np
 import umap
@@ -17,17 +17,27 @@ train_inputs = np.reshape(train_inputs, (rows, height*width))
 print('Reshaped data:', train_inputs.shape)
 
 mapper = umap.UMAP(set_op_mix_ratio=0.25, random_state=42).fit(train_inputs, y=train_targets)
-data_clusters = DBSCAN(n_jobs=-1).fit_predict(mapper.embedding_)
+data_clusters = hdbscan.HDBSCAN().fit_predict(mapper.embedding_)
 
 # Plotting outlying digits.
 outlying_digits = train_inputs[data_clusters == -1]
 print('Number of outlyings digits found:', outlying_digits.shape[0])
 
-fig, axes = plt.subplots(4, 8, figsize=(10,10))
+fig, axes = plt.subplots(5, 6, figsize=(10, 10))
 for i, ax in enumerate(axes.flatten()):
-    ax.imshow(outlying_digits[i].reshape((28,28)))
+    ax.imshow(outlying_digits[i].reshape((28, 28)))
 plt.tight_layout()
 plt.title('Outlying digits')
 plt.show()
 
+# Plotting normal digits vs outlier digits.
+clear_digits = mapper.embedding_[data_clusters != -1]
+outlying_digits = mapper.embedding_[data_clusters == -1]
 
+# Plotting dataset.
+_, ax = plt.subplots()
+color = train_targets.astype(int)
+plt.scatter(clear_digits[:, 0], clear_digits[:, 1], c='blue', cmap='Spectral', s=0.1)
+plt.scatter(outlying_digits[:, 0], outlying_digits[:, 1], c='red', cmap='Spectral', s=0.1)
+ax.set_title('MNIST Dataset Visualization', fontsize=16)
+plt.show()
